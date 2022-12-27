9754ba443bf5a9715d1ef83c074f1ffed2fbea31
Orest Andrusyshyn <orest.andrusyshyn@ucu.edu.ua>
2022-12-28 00:01:49 +0200

    deleting c

--- include/boot_sector.h
+++
@@ -1,7 +0,0 @@
-#ifndef CHECKING_FAT_BOOT_SECTOR_H
-#define CHECKING_FAT_BOOT_SECTOR_H
-
-#include "structs.h"
-
-void printBootSector(BSStruct* bootStruct);
-#endif //CHECKING_FAT_BOOT_SECTOR_H
--- include/root_dir.h
+++
@@ -1,8 +0,0 @@
-#include "structs.h"
-
-#ifndef CHECKING_FAT_ROOT_DIR_H
-#define CHECKING_FAT_ROOT_DIR_H
-
-// this function was taken from https://students.cs.byu.edu/~cs345ta/labs/P6-FAT%20Supplement.html
-void printRootDir(int fd, uint32_t rootOffset, BSStruct *bootSector, unsigned char FAT[]);
-#endif //CHECKING_FAT_ROOT_DIR_H
